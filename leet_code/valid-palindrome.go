9e47de7d1ba8bf89eadba84a05ce59ee450e4061
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2024-03-23 14:06:39 -0400

    Valid Palindrome

---
+++ leet_code/valid-palindrome.go
@@ -0,0 +1,47 @@
+package leet_code
+
+import "unicode"
+
+func isPalindrome(s string) bool {
+	var runes []rune
+
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			runes = append(runes, unicode.ToLower(r))
+		} else if unicode.IsDigit(r) {
+			runes = append(runes, r)
+		}
+	}
+
+	if l := len(runes); l > 0 {
+		for i := 0; i < l/2; i++ {
+			if runes[i] != runes[l-i-1] {
+				return false
+			}
+		}
+	}
+	return true
+
+}
+
+//func isPalindrome(s string) bool {
+//	var runes []rune
+//
+//	for _, r := range s {
+//		if unicode.IsLetter(r) {
+//			runes = append(runes, unicode.ToLower(r))
+//		} else if unicode.IsDigit(r) {
+//			runes = append(runes, r)
+//		}
+//	}
+//
+//	if l := len(runes); l > 0 {
+//		for i := 0; i < l/2; i++ {
+//			if runes[i] != runes[l-i-1] {
+//				return false
+//			}
+//		}
+//	}
+//	return true
+//
+//}
