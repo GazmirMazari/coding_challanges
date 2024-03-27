39ae89e70aefe632f4b703712daa8e74d53686c7
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2024-03-27 05:20:45 -0400

    Implement binary search in go

--- algorythmns/binarySearch.go
+++ algorythmns/binarySearch.go
@@ -4,23 +4,21 @@ func binarySearch(list []int, target int) int {
 
 	low := 0
 	high := len(list) - 1
-	mid := (low + high) / 2
-
-	guess := list[mid]
 
 	for low <= high {
+		mid := (low + high) / 2
+		guess := list[mid]
+
 		if guess == target {
-			return guess
-		}
-		if guess < target {
-			low = mid + 1
+			return mid
 		}
 
 		if guess > target {
-			high = mid + 1
+			high = mid - 1
+		} else {
+			low = mid + 1
 		}
-
 	}
+	return -1 // If item not found in the list
 
-	return -1
 }
